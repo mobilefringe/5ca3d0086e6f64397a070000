995f52a1a62fa670b0f705ec1121429fbb4a05a8
alex@mobilefringe.com @ CodeCloud <alex@mobilefringe.com @ CodeCloud>
2019-04-02 17:26:36 -0400

    Plaza Paseo Real 2.0 1.0  /main.js

--- main.js
+++ main.js
@@ -102,7 +102,7 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'vuex-router-syn
                             },
                             {
                                 name: "Dining",
-                                href: "/stores?category=dining_full_service"
+                                href: "/dine"
                             }
                         ]
                     },
