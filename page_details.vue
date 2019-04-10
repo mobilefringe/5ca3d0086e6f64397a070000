e10eccd5878fb3cedf9bc59ef079852a6dd9ac8d
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-04-10 14:42:10 -0400

    Plaza Paseo Real 2.0 1.0  /page_details.vue

--- page_details.vue
+++ page_details.vue
@@ -38,6 +38,14 @@
             },
             created() {
                 this.updateCurrentPage(this.id);
+                var temp_repo = this.findRepoByName('Newsletter Banner');
+                    if(temp_repo != null && temp_repo != undefined) {
+                        this.pageBanner = temp_repo.images[0];
+                    } else {
+                        this.pageBanner = {
+                            "image_url": "//codecloud.cdn.speedyrails.net/sites/5b71eb886e6f6450013c0000/image/jpeg/1529532304000/insidebanner2.jpg"
+                        }
+                    }
             },
             watch: {
                 $route: function () {
