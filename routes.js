76dbbbe64a54aa757298b2cff7ce3fcf6c8e6c8e
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-04-10 14:16:00 -0400

    Plaza Paseo Real 2.0 1.0  /routes.js

--- routes.js
+++ routes.js
@@ -136,29 +136,29 @@ define([], function () {
                 }
             ]
         },
-         {
-                    path: '/promotions',
-                    component: view('default'),
-                    meta: {
-                        breadcrumb: 'Sales & Promotions',
-                    },
-                    children: [
-                        {
-                            path: '',
-                            component: view('promotions'),
-                            name: 'promotions'
-                        },
-                        {
-                            path: ':id',
-                            component: view('promotion_details'),
-                            meta: {
-                                breadcrumb: 'Promotion Details',
-                            },
-                            name: 'promotionDetails',
-                            props: true
-                        }
-                    ]
+        {
+                path: '/promotions',
+                component: view('default'),
+                meta: {
+                    breadcrumb: 'Sales & Promotions',
                 },
+                children: [
+                    {
+                        path: '',
+                        component: view('promotions'),
+                        name: 'promotions'
+                    },
+                    {
+                        path: ':id',
+                        component: view('promotion_details'),
+                        meta: {
+                            breadcrumb: 'Promotion Details',
+                        },
+                        name: 'promotionDetails',
+                        props: true
+                    }
+                ]
+            },
         {
             path: '/events-and-promotions',
             component: view('default'),
