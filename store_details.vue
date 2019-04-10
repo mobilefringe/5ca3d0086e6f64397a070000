9c3b873ddd77a072ba87f79c4a02ea165ee8794a
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-04-10 16:25:30 -0400

    Plaza Paseo Real 2.0 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -49,7 +49,7 @@
                         </div>
                         <div class="col-md-8">
                             <div id="map" class="margin_20">
-                                <mapplic-png-map ref="pngmap_ref" :height="314" :hovertip="true" :storelist="allStores" :floorlist="floorList" :svgWidth="property.map_image_width" :svgHeight="property.map_image_height" @updateMap="updatePNGMap"></mapplic-png-map>
+                                <mapplic-png-map ref="pngmap_ref" :height="314" :hovertip="true" :storelist="allStores" :floorlist="floorList" :svgWidth="property.map_image_width" :svgHeight="property.map_image_height" @updateMap="updatePNGMap" class="store_details_map"></mapplic-png-map>
                             </div>
                             <div class=" margin_30 store_details_desc" v-html="currentStore.rich_description"></div>
                             <div v-if="currentStore.events">
@@ -131,7 +131,7 @@
 </template>
 
 <style>
-    .mapplic-popup-link {
+    .store_details_map .mapplic-popup-link {
         display: none !important;
     }
 </style>
