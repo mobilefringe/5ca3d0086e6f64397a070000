37c4c5106ac0d709abc277e80066213cba05c411
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-04-10 15:25:25 -0400

    Plaza Paseo Real 2.0 1.0  /newsletter.vue

--- newsletter.vue
+++ newsletter.vue
@@ -13,7 +13,7 @@
                         <div class="col-md-12">
                             <breadcrumb></breadcrumb>
                             <div v-if="pageContent" v-html="pageContent.body"></div>
-                            <form class="newsletter_form form-horizontal" action="//mobilefringe.createsend.com/t/d/s/xhyhjk/" method="post" @submit.prevent="validateBeforeSubmit">
+                            <form class="newsletter_form form-horizontal" action="//mobilefringe.createsend.com/t/d/s/xhyhjk/" method="post">
                                 <div class="row">
                                     <div class="col-sm-6" >
                                         <label for="fieldgijiid" class="accessibility">First Name</label>
