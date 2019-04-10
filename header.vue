b94d6cb3da30dc3020644da2e8dcd9a82379ac58
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-04-10 14:13:57 -0400

    Plaza Paseo Real 2.0 1.0  /header.vue

--- header.vue
+++ header.vue
@@ -83,6 +83,7 @@
                     windowWidth: 0,
                     scrollY: false,
                     siteInfo: site,
+                    showDropDown: null
                 }
             },
             props:['menu_items', 'social_media'],
