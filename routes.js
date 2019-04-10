df3f5636cecdc7273f5064c0d7ab60bd7ff0924c
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-04-10 14:15:54 -0400

    Plaza Paseo Real 2.0 1.0  /routes.js

--- routes.js
+++ routes.js
@@ -136,6 +136,29 @@ define([], function () {
                 }
             ]
         },
+         {
+                    path: '/promotions',
+                    component: view('default'),
+                    meta: {
+                        breadcrumb: 'Sales & Promotions',
+                    },
+                    children: [
+                        {
+                            path: '',
+                            component: view('promotions'),
+                            name: 'promotions'
+                        },
+                        {
+                            path: ':id',
+                            component: view('promotion_details'),
+                            meta: {
+                                breadcrumb: 'Promotion Details',
+                            },
+                            name: 'promotionDetails',
+                            props: true
+                        }
+                    ]
+                },
         {
             path: '/events-and-promotions',
             component: view('default'),
@@ -225,29 +248,6 @@ define([], function () {
                                 breadcrumb: 'Centre Map',
                             },
                             name: 'map'
-                        }
-                    ]
-                },
-                {
-                    path: '/promotions',
-                    component: view('default'),
-                    meta: {
-                        breadcrumb: 'Sales & Promotions',
-                    },
-                    children: [
-                        {
-                            path: '',
-                            component: view('promotions'),
-                            name: 'promotions'
-                        },
-                        {
-                            path: ':id',
-                            component: view('promotion_details'),
-                            meta: {
-                                breadcrumb: 'Promotion Details',
-                            },
-                            name: 'promotionDetails',
-                            props: true
                         }
                     ]
                 },
