d538b19f64fb1a09e630902155b9aaaf1d336cf7
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-04-12 11:07:37 -0400

    Plaza Paseo Real 2.0 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -271,7 +271,7 @@
             methods: {
                 loadData: async function() {
                     try {
-                        let results = await Promise.all([this.$store.dispatch("getData", "banners"), this.$store.dispatch("getData","promotions"), this.$store.dispatch("getData", "events"), this.$store.dispatch('LOAD_PAGE_DATA', {url: "https://peninsula.mallmaverick.com/api/v4/peninsula/social.json"})]);
+                        let results = await Promise.all([this.$store.dispatch("getData", "banners"), this.$store.dispatch("getData","promotions"), this.$store.dispatch("getData", "events"), this.$store.dispatch('LOAD_PAGE_DATA', {url: "https://paseoreal.mallmaverick.com/api/v4/paseoreal/social.json"})]);
                         return results;
                     } catch(e) {
                         console.log("Error loading data: " + e.message);    
