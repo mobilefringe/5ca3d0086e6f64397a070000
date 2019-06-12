6febaf3cca37ba0d7006a614e8a5242efa72b2b3
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-06-12 11:29:49 -0400

    Plaza Paseo Real 2.0 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -38,7 +38,7 @@
                 </div>
                 <messages-component></messages-component>
                 <div class="main_container">
-                    <h2 class="home_title center" v-if="featuredItems.length">Events & Promotions</h2>
+                    <h2 class="home_title center" v-if="featuredItems.length > 0">Events & Promotions</h2>
                     <div class="row margin_40 home_events" v-if="featuredItems.length">
                         <div class="col-sm-4" v-for="item in featuredItems">
                     	    <div v-if="item.eventable_type" class="feature_item_container">
