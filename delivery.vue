d8d7eeeea56fd8d4fbbac9ff58810242ff704c87
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2020-03-11 23:25:23 -0400

    Plaza Paseo Real 2.0 1.2  /delivery.vue

--- delivery.vue
+++ delivery.vue
@@ -119,7 +119,7 @@
                     toggleText: "Display as List",
                     logoView: true,
                     listView: false,
-                    deliveryFilter: 8875
+                    deliveryFilter: 8912
                 }
             },
             created (){
